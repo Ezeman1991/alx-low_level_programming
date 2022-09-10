05c89379fef03ccb836e163f05badcfd9506f4d2
Wisdom Eze <wisdomezeh1@yahoo.com>
2022-09-10 15:57:19 -0700

    “Good”

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#Include <stdio.h>
+#include <stdio.h>
 int main(void)
 {
- puts("programming is like building a multilingual puzzle")
- return (0)
+ puts("programming is like building a multilingual puzzle");
+ return (0);
 } 
