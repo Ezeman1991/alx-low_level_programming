0a443f7402449d7bb118578cdf840d7ba695c195
Wisdom Eze <wisdomezeh1@yahoo.com>
2022-09-12 12:23:52 -0700

    gone

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int a:
+int main() {
+	int a: 
 	long int b;
 	long long int c;
 	char d;
 	float f;
 
+	// Size of Variables
 	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(c));
 	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(b));
