44d22f88ad3201a9e6e094f984ccaedbf8eed32f
Wisdom Eze <wisdomezeh1@yahoo.com>
2022-09-12 13:41:15 -0700

    gone

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main() {
+int main(void) {
 	int a;
 	long int b;
 	long long int c;
@@ -8,7 +8,7 @@ int main() {
 
 	// Size of Variables
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	priintf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 	print("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
