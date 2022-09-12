63873ac85aac5fcb76cee9ef41e86e8dd475b19f
Wisdom Eze <wisdomezeh1@yahoo.com>
2022-09-12 12:41:16 -0700

    gone

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,11 @@ int main() {
 	float f;
 
 	// Size of Variables
-	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(c));
-	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu bytes(s)\n",(unsigned long)sizeof(c));
-	print("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+	printf("Size of char: %ld byte(s)\n", sizeof(d));
+	priintf("Size of int: %ld byte(s)\n", sizeof(c));
+	printf("Size of long int: %ld byte(s)\n", sizeof(b));
+	printf("Size of long long int: %ld bytes(s)\n", sizeof(c));
+	print("Size of float: %ld byte(s)\n", sizeof(f));
+
 	 return (0);
 }
