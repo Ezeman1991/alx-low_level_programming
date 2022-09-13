4d9eff53c040558f80158bde40d9a9fa33e8aa10
Wisdom Eze <wisdomezeh1@yahoo.com>
2022-09-12 17:40:01 -0700

    it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  * Description: "A C program that prints building a multilingual puzzle"
  * Return: Always 0 (Success)
  */
-int main()
-String result = str.trim ();
+int main(void)
+result = str.trim ();
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0)
